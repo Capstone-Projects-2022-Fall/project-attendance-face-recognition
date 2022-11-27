8d6e86274ed4d543416c830f09819034911b4802
Shiv <tug94429@temple.edu>
2022-11-26 21:35:40 -0600

    Forced the user to upload at least 5 images and provided them with a redirect to Canvas once complete

--- face_recognition/src/pages/Registration/components/RegistrationComponent.js
+++ face_recognition/src/pages/Registration/components/RegistrationComponent.js
@@ -2,12 +2,21 @@ import React, {Component, Fragment} from "react";
 import {Button, Card, DialogActions, TextField} from "@mui/material";
 import Box from "@mui/material/Box";
 import {NavLink} from "react-router-dom";
+import {Alert, AlertTitle} from "@mui/material";
 
 class RegistrationComponent extends Component{
 
+    state = {
+        numImagesUploaded : 0
+    }
+
     submitRequest = ()=>{
         this.props.handleRequestSubmit()
+        this.setState({
+	    numImagesUploaded : this.state.numImagesUploaded + 1
+	})
     }
+
     render() {
         return(
             <Fragment>
@@ -35,6 +44,14 @@ class RegistrationComponent extends Component{
                                     Upload
                                 </Button>
                             </DialogActions>
+			    <div className={"card"}>
+				{
+				    <Alert severity="warning">
+					<AlertTitle>Instructions</AlertTitle>
+					Please upload at least 5 images of yourself for proper recognition.
+				    </Alert>
+				}
+			    </div>
                         </Box>
                 </Card>
                 <Box
@@ -47,9 +64,26 @@ class RegistrationComponent extends Component{
                         bgcolor: 'background.paper',
                     }}
                 >
-                    <Button variant="contained" color={"info"} component={NavLink} to="/">
-                        Go Back Home
-                    </Button>
+                    <div className={"card"}>
+			{
+			    this.state.numImagesUploaded === 5 ?
+				(
+			    	    <Fragment>
+					<Alert
+				    	    severity={"success"}
+				    	    action={
+						<Button color={"inherit"}><a href="https://3.235.17.130">
+					    	    Return to Canvas
+						</a></Button>
+				   	    }
+					>
+				    	    <AlertTitle>Registration Complete!</AlertTitle>
+				    	    You are now ready to start taking attendance.
+					</Alert>
+			    	    </Fragment>
+				):<Fragment></Fragment>
+			}
+		    </div>
                 </Box>
             </Fragment>
         )
